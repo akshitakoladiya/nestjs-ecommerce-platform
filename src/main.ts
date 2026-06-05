bd5fdc20dc552d48504a5d1a769b25505982c004
Akshita Koladiya <akshitakoladiya@gmail.com>
2026-06-05 21:31:52 +0200

    Add main application files

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.use(helmet());
+  app.enableCors({ origin: '*', credentials: true });
+  app.enableVersioning({ type: VersioningType.URI, prefix: 'api/v' });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce API')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = configService.get('PORT') || 3000;
+  await app.listen(port);
+  console.log(`Server running on port ${port}`);
+}
+
+bootstrap();
